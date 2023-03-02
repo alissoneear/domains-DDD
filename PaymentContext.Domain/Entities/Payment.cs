abb38d1d70e300836fb40e281fddb9771c65e51d
Alisson Lourenço <alissoneear@gmail.com>
2023-03-02 19:12:21 -0300

    Add project files.

---
+++ PaymentContext.Domain/Entities/Payment.cs
@@ -0,0 +1,20 @@
+﻿namespace PaymentContext.Domain.Entities;
+public class Payment
+{
+
+}
+
+public class BoletoPayment
+{
+
+}
+
+public class CreditCardPayment
+{
+
+}
+
+public class PayPalPayment
+{
+
+}
