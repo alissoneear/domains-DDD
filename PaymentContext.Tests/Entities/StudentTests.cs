4e585c1938d07f51a8511acd6405eb3694f4a20e
alissoneear <alissoneear@gmail.com>
2023-03-03 00:15:03 -0300

    fixing domains

---
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,14 @@
+﻿using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests.Entities;
+[TestClass]
+public class StudentTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var student = new Student("Alisson", "Silva", "41182779808", "alissoneear@gmail.com");
+        var subscription = new Subscription();
+        student.AddSubscription(subscription);
+    }
+}
